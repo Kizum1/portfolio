f46ee4cc0a04d480d352e967fcaede8d74ea47bc
Jay <jaychong1203@gmail.com>
2023-12-17 12:58:21 -0800

    changed icons

--- personal-portfolio/src/components/Tech.js
+++ personal-portfolio/src/components/Tech.js
@@ -1,24 +1,25 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
+
 import {
-  DiGit,
   DiJavascript1,
   DiPython,
   DiReact,
 } from "react-icons/di";
+
 import { FaBootstrap } from "react-icons/fa";
 import {
   SiCss3,
   SiHtml5,
   SiTailwindcss,
-  SiVisualstudiocode,
+  SiGnubash,
+  SiPowershell
 } from "react-icons/si";
 
 
 export const Techstack = () => {
   return (
-    
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <section className='techstackHeader'><h2>Languages / Frameworks</h2></section>
       <Col xs={4} md={2} className="tech-icons">
@@ -46,10 +47,10 @@ export const Techstack = () => {
         <SiTailwindcss />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
+        <SiGnubash />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
+        <SiPowershell />
       </Col>
     </Row>
   );
