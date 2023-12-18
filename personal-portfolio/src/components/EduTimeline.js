333d4a683096f5dfb28bb255085a0805850d4381
Jay <jaychong1203@gmail.com>
2023-12-17 17:47:26 -0800

    updated edu timeline

--- personal-portfolio/src/components/EduTimeline.js
+++ personal-portfolio/src/components/EduTimeline.js
@@ -1,30 +1,31 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import Yale from '../assets/img/yale.png';
+import pan from '../assets/img/pan.png';
 import DVC from '../assets/img/dvc.png';
 import UCB from '../assets/img/ucb.png';
+import cp from '../assets/img/cp.png';
 
 export const EduTimeline = () => {
   return (
     <div className="timeline-container">
-      <section className='project'><h2>Education</h2></section>
+      <section className='project'><h2>Education & Experiences</h2></section>
     <VerticalTimeline>
       <VerticalTimelineElement
         id="education"
         className="vertical-timeline-element--education"
         contentStyle={{ background: '#5E92F3', color: '#fff' }}
         contentArrowStyle={{ borderRight: '7px solid #5E92F3' }}
-        date="2018 - 2022"
+        date="2021 - 2022"
         iconStyle={{ background: '#5E92F3', color: '#fff' }}
-        icon={<img src={Yale} alt="Yale Logo" />}
+        icon={<img src={pan} alt="Yale Logo" />}
       >
-        <h3 className="vertical-timeline-element-title">High School</h3>
-        <h5 className="vertical-timeline-element-subtitle">Yale Secondary</h5>
+        <h3 className="vertical-timeline-element-title">Cybersecurity Bootcamp</h3>
+        <h5 className="vertical-timeline-element-subtitle">Palo Alto Networks</h5>
         <p>
-          Dogwood Diploma <br />
-          Palo Alto Networks Academy <br />
-          94% Average <br />
+          Simulated breaches in virtual environments <br />
+          Proficient in Strata, Prisma, and Cortex automation <br />
+          PCCET certified for entry-level cybersecurity per NIST <br />
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
@@ -39,8 +40,24 @@ export const EduTimeline = () => {
         <h5 className="vertical-timeline-element-subtitle">Diablo Valley College</h5>
         <p>
           Associates of Computer Engineering <br />
-          GDSC Lead <br />
+          Google Developers Student Lead <br />
           Academic Honors: 4.0 GPA <br />
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--education"
+        contentStyle={{ background: '#132ed1', color: '#ffffff' }}
+        contentArrowStyle={{ borderRight: '7px solid #68C7D1' }}
+        date="2023"
+        iconStyle={{ background: '#68C7D1', color: '#fff' }}
+        icon={<img src={cp} alt="CodePath Logo" />}
+      >
+        <h3 className="vertical-timeline-element-title">Intermediate Cybersecurity Student</h3>
+        <h5 className="vertical-timeline-element-subtitle">CodePath Org</h5>
+        <p>
+        SOC operations, incident handling, threat analysis expertise. <br />
+        Proficient in endpoint monitoring, log analysis, network security. <br />
+        Skilled in threat intelligence sharing with frameworks <br />
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
@@ -51,28 +68,12 @@ export const EduTimeline = () => {
         iconStyle={{ background: '#68C7D1', color: '#fff' }}
         icon={<img src={UCB} alt="UCB Logo" />}
       >
-        <h3 className="vertical-timeline-element-title">Undergraduate Student</h3>
-        <h5 className="vertical-timeline-element-subtitle">X University</h5>
+        <h3 className="vertical-timeline-element-title">Undergraduate/Graduate Student</h3>
+        <h5 className="vertical-timeline-element-subtitle">UC Berkeley</h5>
         <p>
           Bachelors of Electrical Engineering & Computer Science <br />
-          X Company Intern <br />
-          X GPA <br />
-        </p>
-      </VerticalTimelineElement>
-      <VerticalTimelineElement
-        className="vertical-timeline-element--education"
-        contentStyle={{ background: '#132ed1', color: '#ffffff' }}
-        contentArrowStyle={{ borderRight: '7px solid #68C7D1' }}
-        date="2026-2027"
-        iconStyle={{ background: '#68C7D1', color: '#fff' }}
-        icon={<img src={UCB} alt="UCB Logo" />}
-      >
-        <h3 className="vertical-timeline-element-title">Graduate Student</h3>
-        <h5 className="vertical-timeline-element-subtitle">X University</h5>
-        <p>
           Masters of Cybersecurity <br />
-          X Research <br />
-          X Company Intern <br />
+          X Company Intern<br />
         </p>
       </VerticalTimelineElement>
     </VerticalTimeline></div>
