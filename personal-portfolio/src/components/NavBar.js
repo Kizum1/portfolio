f0680c777e13eb6f01afc2a48b16674805fb2199
Jay <jaychong1203@gmail.com>
2023-12-24 10:39:27 -0800

    emoji

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -43,7 +43,7 @@ export const NavBar = () => {
               <Nav.Link as={Link} to="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link as={Link} to="#education" className={activeLink === 'education' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('education')}>Education/Experience</Nav.Link>
               <Nav.Link as={Link} to="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
-              <Nav.Link href="https://drive.google.com/file/d/1O2d9gPqxpUNl1g7cEU7bwrC6ZOI2oAE_/view?usp=sharing" target="_blank" rel="noopener noreferrer" download className={activeLink === 'experience' ? 'active navbar-link' : 'navbar-link'}>Resume</Nav.Link>
+              <Nav.Link href="https://drive.google.com/file/d/1O2d9gPqxpUNl1g7cEU7bwrC6ZOI2oAE_/view?usp=sharing" target="_blank" rel="noopener noreferrer" download className={activeLink === 'experience' ? 'active navbar-link' : 'navbar-link'}>Resume 🔗</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
